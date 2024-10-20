9f34e009554f956aa110a450f30bd449d1eaba6c
hybfkuf <fredjoseph863@gmail.com>
2024-10-20 09:50:10 +0800

    upload iamges

--- features/workspaces/components/create-workspace-form.tsx
+++ features/workspaces/components/create-workspace-form.tsx
@@ -11,6 +11,10 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useCreateWorkspace } from '../api/use-create-workspace'
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { LucideImage } from 'lucide-react'
 
 interface props {
   onCancel?: () => void
@@ -18,15 +22,37 @@ interface props {
 
 export const CreateWorkspaceForm = ({ onCancel }: props) => {
   const { mutate, isPending } = useCreateWorkspace()
+  const inputRef = useRef<HTMLInputElement>(null)
   const form = useForm<z.infer<typeof createWorkspaceSchema>>({
     resolver: zodResolver(createWorkspaceSchema),
     defaultValues: {
       name: '',
     },
   })
+
   const onSumbit = (values: z.infer<typeof createWorkspaceSchema>) => {
-    mutate({ json: values })
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : '',
+    }
+    mutate(
+      { form: finalValues },
+      {
+        onSuccess: () => {
+          form.reset()
+          // TODO: redirect to workspaces
+        },
+      },
+    )
   }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      form.setValue('image', file)
+    }
+  }
+
   return (
     <Card className="h-full w-full border-none shadow-none">
       <CardHeader className="flex p-7">
@@ -48,6 +74,54 @@ export const CreateWorkspaceForm = ({ onCancel }: props) => {
                     </FormControl>
                     <FormMessage />
                   </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <div className="flex flex-col gap-y-2">
+                    <div className="flex items-center gap-x-5">
+                      {field.value ? (
+                        <div className="relative size-[72px] overflow-hidden rounded-md">
+                          <Image
+                            src={field.value instanceof File ? URL.createObjectURL(field.value) : field.value}
+                            alt="logo"
+                            fill
+                            className="object-cover"
+                          />
+                        </div>
+                      ) : (
+                        <Avatar className="size-[72px]">
+                          <AvatarFallback>
+                            <LucideImage className="size-[36px] text-neutral-400" />
+                          </AvatarFallback>
+                        </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <p className="text-sm">Workspace Icon</p>
+                        <p className="text-sm text-muted-foreground">JPG, PNG, SVG or JPEG, max 1mb</p>
+                        <input
+                          accept=".jpg, .png, .jpeg, .svg"
+                          type="file"
+                          ref={inputRef}
+                          disabled={isPending}
+                          className="hidden"
+                          onChange={handleImageChange}
+                        />
+                        <Button
+                          type="button"
+                          disabled={isPending}
+                          variant="teritary"
+                          size="xs"
+                          className="mt-2 w-fit"
+                          onClick={() => inputRef.current?.click()}
+                        >
+                          Upload Image
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
                 )}
               />
             </div>
--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -2,17 +2,25 @@ import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { createWorkspaceSchema } from '../schemas'
 import { sessionMiddleware } from '@/lib/session-middleware'
-import { DATABASE_ID, WORKSPACE_ID } from '@/config'
+import { DATABASE_ID, IMAGE_BUCKET_ID, WORKSPACE_ID } from '@/config'
 import { ID } from 'node-appwrite'
 
-const app = new Hono().post('/', zValidator('json', createWorkspaceSchema), sessionMiddleware, async (c) => {
+const app = new Hono().post('/', zValidator('form', createWorkspaceSchema), sessionMiddleware, async (c) => {
   const databases = c.get('databases')
+  const storage = c.get('storage')
   const user = c.get('user')
 
-  const { name } = c.req.valid('json')
+  const { name, image } = c.req.valid('form')
+  let uploadImageUrl: string | undefined
+  if (image instanceof File) {
+    const file = await storage.createFile(IMAGE_BUCKET_ID, ID.unique(), image)
+    const arrayBuffer = await storage.getFilePreview(IMAGE_BUCKET_ID, file.$id)
+    uploadImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString('base64')}`
+  }
   const workspace = await databases.createDocument(DATABASE_ID, WORKSPACE_ID, ID.unique(), {
     name: name,
     userId: user.$id,
+    imageUrl: uploadImageUrl,
   })
 
   return c.json({ data: workspace })
