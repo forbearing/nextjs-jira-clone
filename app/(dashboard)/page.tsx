0036d283cf2bb121d83b349b1fbe91e4946f5ea6
hybfkuf <fredjoseph863@gmail.com>
2024-10-21 22:42:05 +0800

    rename auth/actions.ts -> auth/queries.ts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 'use server'
 
-import { getCurrent } from '@/features/auth/actions'
+import { getCurrent } from '@/features/auth/queries'
 import { getWorkspace } from '@/features/workspaces/actions'
 import { redirect } from 'next/navigation'
 
