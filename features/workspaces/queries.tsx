e66e6f024c04f9ed754b9a5c0455eb153a9bf105
hybfkuf <fredjoseph863@gmail.com>
2024-10-21 22:57:18 +0800

    refactoring workspaces

--- features/workspaces/queries.tsx
+++ features/workspaces/queries.tsx
@@ -1,24 +1,16 @@
 'use server'
 
+import { DATABASE_ID, MEMBER_ID, WORKSPACE_ID } from '@/config'
+import { createSessionClient } from '@/lib/appwrite'
 import { cookies } from 'next/headers'
 import { Account, Client, Databases, Query } from 'node-appwrite'
 import { AUTH_COOKIE } from '../auth/constants'
-import { DATABASE_ID, MEMBER_ID, WORKSPACE_ID } from '@/config'
 import { getMember } from '../members/utils'
 import { Workspace } from './types'
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = await cookies().get(AUTH_COOKIE)
-    if (!session) return { documents: [], total: 0 }
-
-    client.setSession(session.value)
-    const databases = new Databases(client)
-    const account = new Account(client)
+    const { databases, account } = await createSessionClient()
     const user = await account.get()
     const members = await databases.listDocuments(DATABASE_ID, MEMBER_ID, [Query.equal('userId', user.$id)])
     if (members.total === 0) {
@@ -37,16 +29,7 @@ export const getWorkspaces = async () => {
 
 export const getWorkspace = async ({ workspaceId }: { workspaceId: string }) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = await cookies().get(AUTH_COOKIE)
-    if (!session) return null
-
-    client.setSession(session.value)
-    const databases = new Databases(client)
-    const account = new Account(client)
+    const { databases, account } = await createSessionClient()
     const user = await account.get()
     const member = await getMember({ databases, userId: user.$id, workspaceId })
     if (!member) {
