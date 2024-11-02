b1997496c1ec40ef0db7f1a7a9e952f345d41dab
hybfkuf <fredjoseph863@gmail.com>
2024-11-02 13:20:05 +0800

    build workspace analytics

--- app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -2,7 +2,7 @@ import { getCurrent } from '@/features/auth/queries'
 import { redirect } from 'next/navigation'
 import { WorkspaceIdClient } from './client'
 
-export default async ({ params }: { params: { workspaceId: string } }) => {
+export default async () => {
   const user = await getCurrent()
   if (!user) redirect('/sign-in')
 
